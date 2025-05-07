fff1db0e1224192727de224592234254da2cac1c
keylane1355 <keylaneaaraujo@gmail.com>
2025-05-06 22:01:28 -0300

    declaração de variaveis

---
+++ app01.js
@@ -0,0 +1,40 @@
+// Boas práticas
+// Case sensitive
+// Sintaxe básica e comentário
+// Declaração de variáveis(var, let, const)
+// Console e Debug
+
+// Operadores
+//     Operador Atribuição
+//     Operadores aritméticos
+//     Operadores relacionais
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+//     Laços de repetição
+
+// Arrays 
+let nome = "keylane"
+let idade = 22
+
+// Funções básicas
+// Escopo
+
+
+// Alterar conteúdo e atributos *
+// incluir js em página html *
+// Eventos *
+// DOM *
+// Objeto
+// parâmetros e retorno*
+
+// Programação assíncrona***
+// API Fetch ***
+
+// JSON ****
+// funções modernas (arrow functions) ****
+
+
+// Spread e REST  ************
+ 
