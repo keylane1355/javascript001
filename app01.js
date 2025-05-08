4d4159a10f5a6c06ffdd44d57a0155c2842514f4
keylane1355 <keylaneaaraujo@gmail.com>
2025-05-08 20:19:01 -0300

    operadores

--- app01.js
+++ app01.js
@@ -5,10 +5,20 @@
 // Console e Debug
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
+//     Operador Atribuição (=)
+//     Operadores aritméticos(+,-,/,*)
+//     Operadores relacionais(>,<,>=, <=, !=, ==)
+
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2)
+console.log(n1==n2) // iigualdade 
+console.log(n1 != n2) //diferente
+console.log(n1>n2) //meior que
+console.log(10<10) // menor que 
 //     Operadores lógicos
+console.log(!10>2)
+console.log("10"+"15")
 
 // Estrutura
 //     Estrutura de controle/decisão
